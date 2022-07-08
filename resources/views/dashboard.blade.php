7c0956065d8c57c5e586b7e7b9162a40c1774193
Gabriel Lencina <gabriellencina831@gmail.com>
2022-07-07 22:10:31 -0300

    novo commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('title', 'Meus pedidos')
+
+@section('content')
+
+@endsection
---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+<img src="/img/brudam.png" alt="brudam logo" width="250">
+</a>
